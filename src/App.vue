69d036e18fb5d5744e49cfeebfa2d49448b16c35
Christian <ChrisWcs@users.noreply.github.com>
2017-09-20 21:09:16 -0600

    Made progress on Calculator App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
-    <h1>{{count}}</h1>
-    <button @click="increment">+</button>
+    <div class="FlexCol">
+      <div>
+        <h1 class="Display">{{total}}</h1>
+      </div>
+      <div class="FlexRow">
+        <button class="Button" @click="numberPress(7)">7</button>
+        <button class="Button" @click="numberPress(8)">8</button>
+        <button class="Button" @click="numberPress(9)">9</button>
+        <button class="Button" @click="add()">+</button>
+      </div>
+      <div class="FlexRow">
+        <button class="Button" @click="numberPress(4)">4</button>
+        <button class="Button" @click="numberPress(5)">5</button>
+        <button class="Button" @click="numberPress(6)">6</button>
+        <button class="Button" >-</button>
+      </div>
+      <div class="FlexRow">
+        <button class="Button" @click="numberPress(1)">1</button>
+        <button class="Button" @click="numberPress(2)">2</button>
+        <button class="Button" @click="numberPress(3)">3</button>
+        <button class="Button">_</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,24 +31,52 @@ export default {
   name: 'app',
   data () {
     return {
-      count: 1
+      number: 0,
+      total: 0
     }
   },
   methods: {
-    increment () {
-      this.count++
+    numberPress (temp) {
+      this.number = temp
+    },
+    add () {
+      this.total += this.number
+      this.number = 0
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .Display{
+    font-family: Arial;
+  }
+
+  .Button{
+    border: solid black 1px;
+    padding: 10px;
+    background: white;
+    margin: 2px;
+    font-family: Arial;
+    font-size: 20px;
+  }
+
+  :hover.Button{
+    background: black;
+    color: white;
+  }
+
+  .FlexRow{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .FlexCol{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
